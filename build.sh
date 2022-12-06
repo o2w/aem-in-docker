7c5631976418c9425848cc4fee77b794a7725f6d
user <>
2022-12-06 23:10:20 +0900

    rename base image to 6.5.0

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t aem-base-image ./aem-base-image
+docker build -t aem6.5.0 ./aem6.5.0
 docker build -t default-auther ./author
 docker build -t default-publish ./publish
 docker build -t default-dispatcher ./dispatcher
