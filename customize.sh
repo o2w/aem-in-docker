c2fd87ec6731161d5d7605ecd8b41ba2d510f4c8
user <>
2022-12-06 23:43:45 +0900

    bugs, update

--- customize.sh
+++ customize.sh
@@ -7,7 +7,8 @@ cp -rav ./publish custom/$1/
 cp -rav ./dispatcher custom/$1/
 cp -avu ./docker-compose.yml custom/$1/
 cp -avu ./disposable.sh custom/$1/
+cp -avu ./install_packages.sh custom/$1/
 
 cd custom/$1/
-sed -e -i '.bak' "s|default\-|${1}\-|g" ./disposable.sh
-sed -e -i '.bak' "s|default\-|${1}\-|g" ./docker-compose.yml
+sed -i.back -e  's|default\-|'"${1}"'\-|g' ./disposable.sh
+sed -i.back -e  's|default\-|'"${1}"'\-|g' ./docker-compose.yml
